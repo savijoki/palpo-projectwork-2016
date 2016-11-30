ec0fd7fa37d9b704076a0b13e8ecafa456646ccb
Tuomas Aho <tuomas.a.aho@student.tut.fi>
2016-11-30 22:52:11 +0200

    Better local storage if user searches with exactly same search query than before.

--- moviefinder/rest_api/viewsets.py
+++ moviefinder/rest_api/viewsets.py
@@ -34,33 +34,41 @@ class SearchByTitle(APIView):
         sid = transaction.savepoint()
         query = str(query).lower()
         try:
-            omdb_res = requests.get('http://www.omdbapi.com/?t=%s&y=&plot=full&r=json' % query)
-            omdb_res_json = omdb_res.json()
+            movie_local = Movie.objects.filter(searchquery__query=query)
+            imdbId = title = ""
+            if not movie_local.exists():
+                omdb_res = requests.get('http://www.omdbapi.com/?t=%s&y=&plot=full&r=json' % query)
+                if  int(omdb_res.status_code) == 200:
+                    omdb_res_json = omdb_res.json()
+                    title = omdb_res.json()['imdbID']
+                    imdbId = omdb_res_json['Title']
+                else:
+                    raise RequestException()
+            else:
+                title = movie_local[0].title
+                imdbId = movie_local[0].imdbid
             query_db = SearchQuery(query=query)
-            if int(omdb_res.status_code) == 200:
-                imdbId = omdb_res_json['imdbID']
-                title = omdb_res_json['Title']
-                movie_db, created = Movie.objects.get_or_create(imdbid=imdbId,title=title)
-                query_db.movie = movie_db
-                if created or count > len(Trailer.objects.filter(movie=movie_db)):
-                    myapifilms_res = requests.get(
-                        'http://www.myapifilms.com/trailerAddict/taapi?idIMDB=%s&token=%s&featured=&count=%d&credit=&format=json' %
-                        (imdbId, myapifilms_token, count)).json()
-                    try:
-                        trailers = myapifilms_res['data']['trailer']
-                        for trailer in trailers:
-                            try:
-                                trailer_db = Trailer(movie=movie_db, embed=trailer['embed'])
-                                trailer_db.save()
-                            except IntegrityError:
-                                #unique constraint violation
-                                pass
-                    except KeyError as e:
-                        pass
-                query_db.save()
-                transaction.savepoint_commit(sid)
-                serializer = MovieSerializer(movie_db)
-                return Response(serializer.data)
+            movie_db, created = Movie.objects.get_or_create(imdbid=imdbId,title=title)
+            query_db.movie = movie_db
+            if created or count > len(Trailer.objects.filter(movie=movie_db)):
+                myapifilms_res = requests.get(
+                    'http://www.myapifilms.com/trailerAddict/taapi?idIMDB=%s&token=%s&featured=&count=%d&credit=&format=json' %
+                    (imdbId, myapifilms_token, count)).json()
+                try:
+                    trailers = myapifilms_res['data']['trailer']
+                    for trailer in trailers:
+                        try:
+                            trailer_db = Trailer(movie=movie_db, embed=trailer['embed'])
+                            trailer_db.save()
+                        except IntegrityError:
+                            #unique constraint violation
+                            pass
+                except KeyError as e:
+                    pass
+            query_db.save()
+            transaction.savepoint_commit(sid)
+            serializer = MovieSerializer(movie_db)
+            return Response(serializer.data)
         except RequestException as e:
             print(e)
             print("Connection problem to external API")
@@ -80,33 +88,42 @@ class SearchByImdbId(APIView):
             return JsonResponse(json.dumps({}), safe=False, status=400)
         sid = transaction.savepoint()
         try:
-            omdb_res = requests.get('http://www.omdbapi.com/?i=%s&y=&plot=full&r=json' % imdbId)
-            omdb_res_json = omdb_res.json()
+            movie_local = Movie.objects.filter(searchquery__query=imdbId)
+            pprint(movie_local)
+            imdbId = title = ""
+            if not movie_local.exists():
+                omdb_res = requests.get('http://www.omdbapi.com/?i=%s&y=&plot=full&r=json' % imdbId)
+                if int(omdb_res.status_code) == 200:
+                    omdb_res_json = omdb_res.json()
+                    title = omdb_res.json()['imdbID']
+                    imdbId = omdb_res_json['Title']
+                else:
+                    raise RequestException()
+            else:
+                title = movie_local[0].title
+                imdbId = movie_local[0].imdbid
             query_db = SearchQuery(query=imdbId)
-            if int(omdb_res.status_code) == 200:
-                imdbId = omdb_res_json['imdbID']
-                title = omdb_res_json['Title']
-                movie_db, created = Movie.objects.get_or_create(imdbid=imdbId, title=title)
-                query_db.movie = movie_db
-                if created or count > len(Trailer.objects.filter(movie=movie_db)):
-                    myapifilms_res = requests.get(
-                        'http://www.myapifilms.com/trailerAddict/taapi?idIMDB=%s&token=%s&featured=&count=%d&credit=&format=json' %
-                        (imdbId, myapifilms_token, count)).json()
-                    try:
-                        trailers = myapifilms_res['data']['trailer']
-                        for trailer in trailers:
-                            try:
-                                trailer_db = Trailer(movie=movie_db, embed=trailer['embed'])
-                                trailer_db.save()
-                            except IntegrityError:
-                                # unique constraint violation
-                                pass
-                    except KeyError as e:
-                        pass
-                query_db.save()
-                transaction.savepoint_commit(sid)
-                serializer = MovieSerializer(movie_db)
-                return Response(serializer.data)
+            movie_db, created = Movie.objects.get_or_create(imdbid=imdbId, title=title)
+            query_db.movie = movie_db
+            if created or count > len(Trailer.objects.filter(movie=movie_db)):
+                myapifilms_res = requests.get(
+                    'http://www.myapifilms.com/trailerAddict/taapi?idIMDB=%s&token=%s&featured=&count=%d&credit=&format=json' %
+                    (imdbId, myapifilms_token, count)).json()
+                try:
+                    trailers = myapifilms_res['data']['trailer']
+                    for trailer in trailers:
+                        try:
+                            trailer_db = Trailer(movie=movie_db, embed=trailer['embed'])
+                            trailer_db.save()
+                        except IntegrityError:
+                            # unique constraint violation
+                            pass
+                except KeyError as e:
+                    pass
+            query_db.save()
+            transaction.savepoint_commit(sid)
+            serializer = MovieSerializer(movie_db)
+            return Response(serializer.data)
         except RequestException as e:
             print(e)
             print("Connection problem to external API")
