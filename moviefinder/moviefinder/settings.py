69534ece1ae0c044ad24d2bea1c5f0c71add83e2
Aleksi Savijoki <aleksi.savijoki@student.tut.fi>
2016-12-03 20:18:01 +0200

    Added loading gif that can be shown during ajax-requests

--- moviefinder/moviefinder/settings.py
+++ moviefinder/moviefinder/settings.py
@@ -134,5 +134,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join('example_frontend', 'static'),
+)
 
